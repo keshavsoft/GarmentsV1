b9ddf1c7e59815496a7ca2035bba76e76a81d605
keshavsoft <nknnkumar@yahoo.com>
2024-10-21 17:44:08 +0530

    public also added

---
+++ public/Garments/Sales/Generation/ShowAll/ShowAll.js
@@ -0,0 +1,19 @@
+import { StartFunc as ShowAllStartFunc } from "../ShowAll/ToDom/ShowInBody.js";
+import { StartFunc as AddListenersStartFunc } from "../ShowAll/AddListeners.js";
+import { StartFunc as FormLoadFuncsStartFunc } from "./FormLoadFuncs.js";
+import { ConfigObject } from "../../../ApiConfig.js";
+import { KeysObject } from "../ConfigKeys.js";
+
+let jVarCommonKeys = KeysObject.CommonKeys;
+let jVarCommonProjectName = ConfigObject.ProjectName;
+
+let StartFunc = async () => {
+    
+    FormLoadFuncsStartFunc();
+
+    await ShowAllStartFunc({ ...jVarCommonKeys, inProjectName: jVarCommonProjectName });
+
+    AddListenersStartFunc({ ...jVarCommonKeys, inProjectName: jVarCommonProjectName });
+};
+
+StartFunc().then();
