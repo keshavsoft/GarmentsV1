0fc9b76d5356d3e1fb9680d3b158940412faffa3
keshavsoft <nknnkumar@yahoo.com>
2024-10-21 22:21:11 +0530

    SearchForStatus added

--- binV4/SRVoucher/routes.js
+++ binV4/SRVoucher/routes.js
@@ -7,11 +7,13 @@ import { router as Show } from './Show/routes.js';
 import { router as Search } from './Search/routes.js';
 import { router as Filter } from './Filter/routes.js';
 import { router as RowShow } from './RowShow/routes.js';
+import { router as SearchForStatus } from './SearchForStatus/routes.js';
 
 router.use('/Create', Create);
 router.use('/Show', Show);
 router.use('/Search', Search);
 router.use('/Filter', Filter);
 router.use('/RowShow', RowShow);
+router.use('/SearchForStatus', SearchForStatus);
 
 export { router };
