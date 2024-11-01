530dfe8d5685ef1fbf50fba96aca7d20a520924c
Srivasu215 <sri.vasu215@gmail.com>
2024-11-01 14:06:34 +0530

    Stock Reduce

---
+++ public/Garments/AddOns/StockReduce/QrScan/Js/Addlisteners/QrKeyPress/FetchFromFuncs/PostFetch.js
@@ -0,0 +1,16 @@
+import ConfigJson from "../../../Config.json" with { type: "json" };
+
+let StartFunc = async ({ inRowpk }) => {
+    let LocalroutePath = ConfigJson.routePath;
+    let LocalRowpk = inRowpk;
+
+
+    let jVarLocalFetchUrl = `/${LocalroutePath}/Generate/RowShow/${LocalRowpk}`;
+    let response = await fetch(jVarLocalFetchUrl);
+    let data = await response.json();
+
+    return await data;
+};
+
+export { StartFunc };
+
